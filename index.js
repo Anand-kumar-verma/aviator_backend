afa2e06d1f99f2f8c4c0765690a5b65eda9a4584
anand <vermaanand278@gmail.com>
2024-04-04 11:03:40 +0530

    dsf

--- index.js
+++ index.js
@@ -199,7 +199,6 @@ const generatedTimeEveryAfterEveryThreeMinTRX = () => {
     const currentTime = new Date().getSeconds(); // Get the current time
     const timeToSend = currentTime > 0 ? 60 - currentTime : currentTime;
     io.emit("threemintrx", `${min}_${timeToSend}`);
-    console.log("threemintrx", `${min}_${timeToSend}`)
     if (currentTime === 0) {
       min--;
       if (min < 0) min = 2; // Reset min to 2 when it reaches 0
